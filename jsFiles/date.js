5a09916ea7023a981e60728d65c21bb338951d57
Nabil Arbouz <43759350+nabilarbouz@users.noreply.github.com>
2021-02-15 17:39:56 -0800

    Delete date.js

--- jsFiles/date.js
+++
@@ -1,9 +0,0 @@
-const setTodaysDate = () => {
-  let todaysDate = new Date();
-  let dateElement = document.getElementById("todaysDate");
-  dateElement.textContent = `Today's Date: ${
-    todaysDate.getMonth() + 1
-  }/${todaysDate.getDate()}/${todaysDate.getFullYear()}`;
-};
-
-setTodaysDate();
